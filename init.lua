cb43333cbeb58f358f6841da833312ed441c5547
Christopher Klint <christopher.klint@gmail.com>
2026-03-03 17:29:03 +0100

    Exclude .git, node_modules, .next from file search and show gitignored files

--- init.lua
+++ init.lua
@@ -429,7 +429,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', function()
-        builtin.find_files { hidden = true }
+        builtin.find_files { hidden = true, no_ignore = true, file_ignore_patterns = { '%.git/', 'node_modules/', '%.next/' } }
       end, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
